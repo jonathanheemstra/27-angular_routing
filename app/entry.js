aa56f906ebe952fe56b0b5fb8be85302c7759b45
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-01-25 19:30:57 -0800

    build out entry.js file

---
+++ app/entry.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('./scss/main.scss');
+
+const angular = require('angular');
+const pascalcase = require('pascalcase');
+const path = require('path');
+const uiRouter = require('angular-ui-router');
+
+const routesApp = angular.module('routesApp', [uiRouter]);
+
+let context = require.context('./config/', true, /\.js$/);
+
+context.keys().forEach( key => {
+  routesApp.config(context(key));
+});
+
+context = require.context('./view/', true, /\.js$/);
+
+context.keys().forEach( key => {
+  let name = pascalcase(path.basename(key, '.js'));
+  routesApp.controller(name, context(key));
+});
